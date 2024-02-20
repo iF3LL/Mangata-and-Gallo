59a3b9e7fe49721c5c5ff67dec17c3ab61bfbd5a
Sunnwy <78620585+iF3LL@users.noreply.github.com>
2024-02-20 13:19:29 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('shopMG').addEventListener('click', domath);
+function domath() {
+    var q=3;
+    alert('Button clicked! value of q is '+ (++q)); // Replace this with your desired script
+}
